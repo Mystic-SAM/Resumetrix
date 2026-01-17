21757e9c77da8c52ae429a3f7e5c0787b146929f
SANKET MENDHE <sanket.mendhe.033@gmail.com>
2026-01-18 00:15:22 +0530

    Implement resume review components

--- app/routes/Resume.tsx
+++ app/routes/Resume.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summary from "~/components/Summary";
 import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => ([
@@ -44,7 +47,6 @@ const Resume = () => {
     setImageUrl(imageUrl);
 
     setFeedback(data.feedback);
-    console.log({ resumeUrl, imageUrl, feedback: data.feedback });
   }
 
   useEffect(() => {
@@ -62,7 +64,7 @@ const Resume = () => {
       <div className="flex flex-row w-full max-lg:flex-col-reverse">
         <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
           {imageUrl && resumeUrl && (
-            <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+            <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit shadow-lg border-1 border-gray-200">
               <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
                 <img
                   src={imageUrl}
@@ -77,9 +79,9 @@ const Resume = () => {
           <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-              {/* <Summary feedback={feedback} />
+              <Summary feedback={feedback} />
               <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
-              <Details feedback={feedback} /> */}
+              <Details feedback={feedback} />
             </div>
           ) : (
             <img src="/images/resume-scan-2.gif" className="w-full" />
